a3528dd832ee149e397a87ec42ce9059c58906d1
MOBSkuchen <71943003+MOBSkuchen@users.noreply.github.com>
2024-12-25 18:24:23 +0100

    Add hashing

--- src/lib_crypt.rs
+++ src/lib_crypt.rs
@@ -1,7 +1,12 @@
 use sodiumoxide::crypto::pwhash::{argon2id13};
+use sodiumoxide::crypto::hash::hash;
 use sodiumoxide::crypto::secretbox;
 use std::error::Error;
+use std::ffi::OsString;
+use std::fs;
+use std::path::Path;
 use serde::{Serialize, Deserialize};
+use crate::display_error;
 
 #[derive(Serialize, Deserialize)]
 struct EncryptedData {
@@ -33,7 +38,7 @@ fn decrypt(ciphertext: &[u8], nonce: &secretbox::Nonce, key: &secretbox::Key) ->
     Ok(plaintext)
 }
 
-pub fn encrypt_buffer(password: String, buffer: Vec<u8>) -> bincode::Result<Vec<u8>> {
+fn encrypt_buffer(password: String, buffer: Vec<u8>) -> bincode::Result<Vec<u8>> {
     let salt = argon2id13::gen_salt();
     let key = derive_key_from_passphrase(password.as_str(), (&salt).as_ref()).unwrap();
     let (ciphertext, nonce) = encrypt(&*buffer, &key);
@@ -47,7 +52,7 @@ pub fn encrypt_buffer(password: String, buffer: Vec<u8>) -> bincode::Result<Vec<
     bincode::serialize(&encrypted_data)
 }
 
-pub fn decrypt_buffer(password: String, buffer: Vec<u8>) -> Result<Vec<u8>, Box<dyn Error>> {
+fn decrypt_buffer(password: String, buffer: Vec<u8>) -> Result<Vec<u8>, Box<dyn Error>> {
     let deserialized: EncryptedData = bincode::deserialize(&buffer).unwrap();
 
     let deserialized_key = derive_key_from_passphrase(password.as_str(), &deserialized.salt)?;
@@ -57,4 +62,81 @@ pub fn decrypt_buffer(password: String, buffer: Vec<u8>) -> Result<Vec<u8>, Box<
         &secretbox::Nonce::from_slice(&deserialized.nonce).unwrap(),
         &deserialized_key,
     )
+}
+
+pub fn encrypt_file_xx(file_name: String, password: String, replace: bool) {
+    let mut target_file = file_name.clone() + if replace {""} else {".waxe"};
+    let file_buffer_r = fs::read(&file_name);
+    if file_buffer_r.is_err() {
+        display_error("Operation failed because the file can not be read!".to_string());
+        return;
+    }
+    let file_buffer = file_buffer_r.unwrap();
+    let encrypted_r = encrypt_buffer(password, file_buffer);
+    if encrypted_r.is_err() {
+        display_error("Encryption failed: ".to_string() + encrypted_r.unwrap_err().to_string().as_str());
+        return;
+    }
+    let encrypted_buffer = encrypted_r.unwrap();
+    if replace {
+        let rm_file_r = fs::remove_file(&target_file);
+        if rm_file_r.is_err() {
+            display_error(format!("Failed to remove file {}, falling back!", target_file));
+            target_file = target_file + ".waxe"
+        }
+    }
+    if fs::exists(&target_file).expect("Why tho") {
+        display_error("Target file (".to_owned() + &*target_file + " already exists!");
+    }
+    let write_r = fs::write(target_file, encrypted_buffer);
+    if write_r.is_err() {
+        display_error("Could not write encrypted file".to_string());
+    }
+}
+
+pub fn decrypt_file_xx(file_name: String, password: String, replace: bool) {
+    let mut target_file = if replace { file_name.clone() } else { Path::new(&file_name).file_stem().unwrap().to_os_string().into_string().unwrap() + ".waxd" };
+    let file_buffer_r = fs::read(&file_name);
+    if file_buffer_r.is_err() {
+        display_error("Operation failed because the file can not be read!".to_string());
+        return;
+    }
+    let file_buffer = file_buffer_r.unwrap();
+    let decrypted_r = decrypt_buffer(password, file_buffer);
+    if decrypted_r.is_err() {
+        display_error("Decryption failed: ".to_string() + decrypted_r.unwrap_err().to_string().as_str());
+        return;
+    }
+    let decrypted_buffer = decrypted_r.unwrap();
+    if replace {
+        let rm_file_r = fs::remove_file(&target_file);
+        if rm_file_r.is_err() {
+            display_error(format!("Failed to remove file {}, falling back! => {}", target_file, rm_file_r.unwrap_err().to_string()));
+            target_file = target_file + ".waxd"
+        }
+    }
+    if fs::exists(&target_file).expect("Why tho") {
+        display_error("Target file (".to_owned() + &*target_file + ") already exists!");
+    }
+    let write_r = fs::write(target_file, decrypted_buffer);
+    if write_r.is_err() {
+        display_error("Could not write decrypted file".to_string());
+    }
+}
+
+pub fn hash_file(file_name: String, dst_file: OsString) {
+    if fs::exists(&dst_file).expect("Why tho") {
+        display_error("Target file (".to_owned() + dst_file.to_str().unwrap() + ") already exists!");
+    }
+    let file_buffer_r = fs::read(&file_name);
+    if file_buffer_r.is_err() {
+        display_error("Operation failed because the file can not be read!".to_string());
+        return;
+    }
+    let file_buffer = file_buffer_r.unwrap();
+    let hashed = hash(&file_buffer);
+    let write_r = fs::write(dst_file, hashed);
+    if write_r.is_err() {
+        display_error("Could not write decrypted file".to_string());
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,75 +3,16 @@
 mod lib_crypt;
 extern crate native_windows_gui as nwg;
 
-use std::{env, fs};
-use std::path::Path;
+use std::{env, fs, io};
+use std::path::{Path, PathBuf};
 use std::rc::Rc;
 use native_windows_gui::WindowFlags;
-use nwg::CheckBoxState;
-use crate::lib_crypt::{encrypt_buffer, decrypt_buffer};
+use nwg::{CheckBoxState, FileDialogAction};
+use nwg::FileDialogAction::Save;
+use crate::lib_crypt::{encrypt_file_xx, decrypt_file_xx, hash_file};
 
 pub fn display_error(error_string: String) {
     nwg::error_message("Error", error_string.as_str());
-}
-
-fn encrypt_file_xx(file_name: String, password: String, replace: bool) {
-    let mut target_file = file_name.clone() + if replace {""} else {".waxe"};
-    let file_buffer_r = fs::read(&file_name);
-    if file_buffer_r.is_err() {
-        display_error("Operation failed because the file can not be read!".to_string());
-        return;
-    }
-    let file_buffer = file_buffer_r.unwrap();
-    let encrypted_r = encrypt_buffer(password, file_buffer);
-    if encrypted_r.is_err() {
-        display_error("Encryption failed: ".to_string() + encrypted_r.unwrap_err().to_string().as_str());
-        return;
-    }
-    let encrypted_buffer = encrypted_r.unwrap();
-    if replace {
-        let rm_file_r = fs::remove_file(&target_file);
-        if rm_file_r.is_err() {
-            display_error(format!("Failed to remove file {}, falling back!", target_file));
-            target_file = target_file + ".waxe"
-        }
-    }
-    if fs::exists(&target_file).expect("Why tho") {
-        display_error("Target file (".to_owned() + &*target_file + " already exists!");
-    }
-    let write_r = fs::write(target_file, encrypted_buffer);
-    if write_r.is_err() {
-        display_error("Could not write encrypted file".to_string());
-    }
-}
-
-fn decrypt_file_xx(file_name: String, password: String, replace: bool) {
-    let mut target_file = if replace { file_name.clone() } else { Path::new(&file_name).file_stem().unwrap().to_os_string().into_string().unwrap() + ".waxd" };
-    let file_buffer_r = fs::read(&file_name);
-    if file_buffer_r.is_err() {
-        display_error("Operation failed because the file can not be read!".to_string());
-        return;
-    }
-    let file_buffer = file_buffer_r.unwrap();
-    let decrypted_r = decrypt_buffer(password, file_buffer);
-    if decrypted_r.is_err() {
-        display_error("Decryption failed: ".to_string() + decrypted_r.unwrap_err().to_string().as_str());
-        return;
-    }
-    let decrypted_buffer = decrypted_r.unwrap();
-    if replace {
-        let rm_file_r = fs::remove_file(&target_file);
-        if rm_file_r.is_err() {
-            display_error(format!("Failed to remove file {}, falling back! => {}", target_file, rm_file_r.unwrap_err().to_string()));
-            target_file = target_file + ".waxd"
-        }
-    }
-    if fs::exists(&target_file).expect("Why tho") {
-        display_error("Target file (".to_owned() + &*target_file + ") already exists!");
-    }
-    let write_r = fs::write(target_file, decrypted_buffer);
-    if write_r.is_err() {
-        display_error("Could not write decrypted file".to_string());
-    }
 }
 
 fn wnd_main(file_path: String, op: bool) {
@@ -130,10 +71,11 @@ fn wnd_main(file_path: String, op: bool) {
                 },
             E::OnButtonClick =>
                 if &handle == &magic_button {
+                    let password = password_entry.text();
                     if op {
-                        encrypt_file_xx(file_path.clone(), password_entry.text(), replace_checkbox.check_state() == CheckBoxState::Checked);
+                        encrypt_file_xx(file_path.clone(), password, replace_checkbox.check_state() == CheckBoxState::Checked);
                     } else {
-                        decrypt_file_xx(file_path.clone(), password_entry.text(), replace_checkbox.check_state() == CheckBoxState::Checked);
+                        decrypt_file_xx(file_path.clone(), password, replace_checkbox.check_state() == CheckBoxState::Checked);
                     }
                     
                     let _ = &events_window.close();
@@ -146,21 +88,56 @@ fn wnd_main(file_path: String, op: bool) {
     nwg::unbind_event_handler(&handler);
 }
 
+pub fn absolute_path(path: impl AsRef<Path>) -> io::Result<PathBuf> {
+    let path = path.as_ref();
+
+    let absolute_path = if path.is_absolute() {
+        path.to_path_buf()
+    } else {
+        env::current_dir()?.join(path)
+    };
+
+    Ok(absolute_path)
+}
+
+fn hash_crafter(file_path: String) {
+    nwg::init().expect("Failed to init Native Windows GUI");
+    nwg::Font::set_global_family("Segoe UI").expect("Failed to set default font");
+    
+    let x = absolute_path(&file_path).unwrap();
+    let default_path = x.parent().unwrap().to_str().unwrap();
+
+    let mut file_save_dialog = Default::default();
+    nwg::FileDialog::builder()
+        .multiselect(false)
+        .filters("Hash(*.hash;*.sha)|Any(*.*)")
+        .default_folder(default_path)
+        .action(Save)
+        .title("Save hash")
+        .build(&mut file_save_dialog).expect("FileDialog failed to build");
+
+    let result = file_save_dialog.run::<nwg::ControlHandle>(None);
+    if result {
+        hash_file(file_path, file_save_dialog.get_selected_item().unwrap());
+    } else {
+        display_error("Failed - cancelled".to_string())
+    }
+}
+
 fn main() {
-    let args: Vec<_> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
     if (&args).len() != 3 {
         display_error("Invalid argument count. Must be 2!".to_string());
         return;
     }
-    let op = if (&args)[1] == "enc" {true} else if (&args)[1] == "dec" {false} else {
-        display_error(format!("Invalid operation: {}", (&args)[1]));
-        return;
-    };
     let file_path = (&args[2]).to_owned();
     if !fs::exists(file_path.clone()).unwrap() {
         display_error(format!("File not found: {}", file_path));
         return;
     }
-    
-    wnd_main(file_path, op);
+
+    if &args[1] == "enc" {wnd_main(file_path, true)}
+    else if &args[1] == "dec" {wnd_main(file_path, false)}
+    else if &args[1] == "hash" {hash_crafter(file_path)}
+    else { display_error(format!("Invalid operation: {}", (&args)[1])); }
 }
